4f564aec8db99c9c3cc7a6aef2d4aefef56f7280
Tim Mulqueen <tmulqueen@box.com>
2022-09-04 00:29:02 +0200

    fix: add some scanning functionality:x

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 use std::env;
-use std::fs;
+
+use std::fs::File;
 use std::io;
+use std::io::Read;
+
+mod tokens;
+use tokens::{Token, TokenType, parse_chars};
+
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,10 +20,12 @@ fn main() {
     let filename = &args[1];
 
     println!("Reading file {}", filename);
-
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    
+    let contents = load_file(filename.clone());
 
     println!("With text:\n{}", contents);
+
+    run(contents);
 }
 
 fn repl_mode() {
@@ -34,8 +42,54 @@ fn repl_mode() {
     }
 }
 
+fn load_file(file_path: String) -> String {
+    let file_result: Result<File, io::Error> = File::open(file_path);
+
+    let mut file = match file_result {
+        Ok(s) => s,
+        Err(error) => panic!("Failed to open file: {}", error)
+    };
+
+    let mut contents_buffer = String::new();
+    match file.read_to_string(&mut contents_buffer) {
+        Ok(contents) => contents.to_string(),
+        Err(error) => panic!("Failed reading contents of file: {}", error)
+    }
+}
+
 // fn run_file() {}
 
-fn run(line: String) {
-    println!("got line  {}", line)
+fn run(source: String) -> Vec<Token> {
+    let mut tokens: Vec<Token> =  Vec::new();
+
+    // let start: i32 = 0;
+    let source_length: i32 = source.len().try_into().unwrap();
+    let mut line: i32 = 0;
+
+    let mut current: i32 = 0;
+
+    while current >= source_length {
+        scan_token()
+    }
+
+    let hi = source.char_indices();
+    while (Some(hi.next())) {
+
+        let token = parse_chars(hi);
+    }
+
+    tokens.push(Token { token_type: TokenType::EOF, lexeme: "".to_owned(), literal: "".to_owned(), line });
+
+    tokens
 }
+
+fn scan_token() {
+    parse_chars()
+    todo!()
+}
+
+fn report(line: i32, where_claus: String, message: String) {
+    println!("[line: {}]: Error: {}: {}", line, where_claus, message)
+}
+
+
---
+++ src/tokens.rs
@@ -0,0 +1,146 @@
+use std::str::CharIndices;
+
+
+#[derive(Debug)]
+pub enum TokenType {
+    // Single-character tokens
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    COMMA,
+    DOT,
+    MINUS,
+    PLUS,
+    SemiColon,
+    SLASH,
+    STAR,
+
+    // One or Two character tokens
+    BANG,
+    BangEqual,
+    EQUAL,
+    EqualEqual,
+    GREATER,
+    GreatEqual,
+    LESS,
+    LessEqual,
+
+    // Literals
+    IDENTIFIER,
+    STRING,
+    NUMBER,
+
+    // Keywords
+    AND,
+    CLASS,
+    ELSE,
+    FALSE,
+    FUN,
+    FOR,
+    IF,
+    NIL,
+    OR,
+    PRINT,
+    RETURN,
+    SUPER,
+    THIS,
+    TRUE,
+    VAR,
+    WHILE,
+
+    EOF
+}
+
+// impl TokenType {
+   
+// }
+
+pub fn parse_chars(mut char_string: CharIndices) -> Option<TokenType> {
+        
+    let character = char_string.as_str();
+    match character {
+        "(" => Some(TokenType::LeftParen),
+        ")" => Some(TokenType::RightParen),
+        "[" => Some(TokenType::LeftBrace),
+        "]" => Some(TokenType::RightBrace),
+        "," => Some(TokenType::COMMA),
+        "." => Some(TokenType::DOT),
+        "-" => Some(TokenType::MINUS),
+        "+" => Some(TokenType::PLUS),
+        ";" => Some(TokenType::SemiColon),
+        "/" => Some(TokenType::SLASH),
+        "*" => Some(TokenType::STAR),
+        "!" => {
+            let next_character = char_string.next();
+            match next_character {
+                Some((_, characters)) => {
+                    if characters.to_string() == "=" {
+                        // "!=" 
+                        Some(TokenType::BangEqual)
+                    } else {
+                        Some(TokenType::BANG)
+                    }
+                }
+                None => Some(TokenType::EOF)
+            }
+        },
+        
+        "=" => {
+            let next_character = char_string.next();
+            match next_character {
+                Some((_, characters)) => {
+                    if characters.to_string() == "=" {
+                        // "==" 
+                        Some(TokenType::EqualEqual)
+                    } else {
+                        Some(TokenType::EQUAL)
+                    }
+                }
+                None => Some(TokenType::EOF)
+            }
+        }
+        ">" => {
+            let next_character = char_string.next();
+            match next_character {
+                Some((_, characters)) => {
+                    if characters.to_string() == "=" {
+                        // "!=" 
+                        Some(TokenType::GreatEqual)
+                    } else {
+                        Some(TokenType::GREATER)
+                    }
+                }
+                None => Some(TokenType::EOF)
+            }
+        },
+        "<" => {
+            let next_character = char_string.next();
+            match next_character {
+                Some((_, characters)) => {
+                    if characters.to_string() == "=" {
+                        // "!=" 
+                        Some(TokenType::LessEqual)
+                    } else {
+                        Some(TokenType::LESS)
+                    }
+                }
+                None => Some(TokenType::EOF)
+            }
+        },
+        _ => None
+    }
+}
+
+pub struct Token {
+    pub token_type: TokenType,
+    pub lexeme: String,
+    pub literal: String,
+    pub line: i32
+}
+
+impl Token {
+    fn to_string(&self) -> String {
+        format!("{:?} {} {}",self.token_type, self.lexeme, self.literal )
+    }
+}
